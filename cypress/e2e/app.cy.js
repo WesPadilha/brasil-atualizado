e496840afbb6409cddc1fd828ce6b18180b52e78
izaque674 <izaquefs88@hotmail.com>
2024-06-03 17:52:50 -0300

    testesCY

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,57 +1,45 @@
-const { type } = require("@testing-library/user-event/dist/type")
-
 describe('Portal COVID Testes', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('deve digitar "Paraná", apagar e depois digitar "Todos"', () => {
-    // Verifica se o input de filtro está visível
-    cy.get('.filter-input').should('be.visible')
 
-    // Digita "Paraná"
+
+  
+
+  it('deve verificar se o input de filtro está visível', () => {
+    cy.get('.filter-input').should('be.visible')
+  })
+
+  it('deve digitar "Paraná" no input de filtro e depois apagar o texto', () => {
     cy.get('.filter-input').type('Paraná')
     cy.wait(1000)  // Espera explícita para os resultados carregarem
-    
-
-    // Apaga o texto
     cy.get('.filter-input').clear()
-    
-
-    // Verifica se o campo de input está vazio
     cy.get('.filter-input').should('have.value', '')
+  })
 
-    // Digita "Todos"
+  it('deve digitar "Todos" no input de filtro', () => {
     cy.get('.filter-input').type('Todos')
     cy.wait(1000)  // Espera explícita para os resultados carregarem
+  })
 
-    cy.get('[name="region"]')
-    
-
+  it('deve abrir o menu de região e selecionar "Sul"', () => {
+    cy.get('.menu-icon > :nth-child(2)').should('be.visible').click()
+    cy.wait(2000) // Aguarde 2 segundos para garantir que o menu seja exibido
+    cy.get('[name="region"]').should('be.visible')
+    cy.get('[name="region"]').select('Sul')
+    cy.wait(5000) // Aguarde 5 segundos para garantir que a ação seja processada
     
   })
 
-
-  describe('Portal COVID Testes', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    it('deve clicar em um ponto do mapa e verificar informações', () => {
-      // Espera que o mapa carregue
-      cy.get('#map').should('be.visible')
-  
-      // Clica no primeiro ponto do mapa
-      cy.get('svg g circle').first().click()
-      cy.screenshot('mapa-ponto-clicado')  // Tira uma screenshot após clicar no ponto
-  
-      // Verifica se a informação do popup foi exibida
-      cy.get('.leaflet-popup-content').should('be.visible').and('contain', 'Informação Esperada')
-    })
-  })
   
 })
+
+describe('Testes do Mapa', () => {
+  it('deve clicar em um estado e exibir sua informação', () => {
+    cy.visit('/') // Verifique se a URL está correta
+    cy.get('#BR-AC').click() // Clique em um estado específico
+    cy.get('.tooltip').should('be.visible') // Verifique se o tooltip está visível
     
-
-
-
+  })
+})
