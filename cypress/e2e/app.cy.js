8960e332ab7b0447695667f8156e26d7d790bf10
izaque674 <izaquefs88@hotmail.com>
2024-06-03 22:25:26 -0300

    FINAL

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,45 +1,116 @@
-describe('Portal COVID Testes', () => {
+describe('Portal COVID Testes - Filtro', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-
-
-  
-
   it('deve verificar se o input de filtro está visível', () => {
     cy.get('.filter-input').should('be.visible')
+   
   })
 
   it('deve digitar "Paraná" no input de filtro e depois apagar o texto', () => {
     cy.get('.filter-input').type('Paraná')
-    cy.wait(1000)  // Espera explícita para os resultados carregarem
+    cy.screenshot('filter-input-parana-typed')
+    cy.wait(5000) 
+    cy.screenshot('filter-input-parana-typed')
     cy.get('.filter-input').clear()
     cy.get('.filter-input').should('have.value', '')
+   
   })
 
   it('deve digitar "Todos" no input de filtro', () => {
     cy.get('.filter-input').type('Todos')
-    cy.wait(1000)  // Espera explícita para os resultados carregarem
+    cy.wait(5000) 
+    cy.screenshot('filter-input-todos-typed')
+  })
+})
+
+describe('Portal COVID Testes - Menu de Região', () => {
+  beforeEach(() => {
+    cy.visit('/')
   })
 
   it('deve abrir o menu de região e selecionar "Sul"', () => {
     cy.get('.menu-icon > :nth-child(2)').should('be.visible').click()
-    cy.wait(2000) // Aguarde 2 segundos para garantir que o menu seja exibido
+    cy.wait(3000) 
+    
     cy.get('[name="region"]').should('be.visible')
+    
     cy.get('[name="region"]').select('Sul')
-    cy.wait(5000) // Aguarde 5 segundos para garantir que a ação seja processada
+    cy.wait(6000) 
+    cy.screenshot('region-sul-selected')
+  })
+
+  it('deve abrir o menu de região e selecionar "Norte"', () => {
+    cy.get('.menu-icon > :nth-child(2)').should('be.visible').click()
+    cy.wait(3000)
+    
+    cy.get('[name="region"]').should('be.visible')
+   
+    cy.get('[name="region"]').select('Norte')
+    cy.wait(6000)
+    cy.screenshot('region-norte-selected')
+  })
+
+  it('deve abrir o menu de região e selecionar "Centro-Oeste"', () => {
+    cy.get('.menu-icon > :nth-child(2)').should('be.visible').click()
+    cy.wait(3000)
+    
+    cy.get('[name="region"]').should('be.visible')
+    
+    cy.get('[name="region"]').select('Centro-Oeste')
+    cy.wait(6000)
+    cy.screenshot('region-centro-oeste-selected')
+  })
+})
+
+describe('Portal COVID Testes - Interação com o Mapa', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('deve clicar em um estado no mapa e exibir sua informação', () => {
+    cy.get('#BR-AC').click()
+    cy.wait(5000) 
+    cy.screenshot('map-state-AC-clicked')
+    cy.get('.tooltip').should('exist') 
+    cy.get('.tooltip').should('be.visible')
+  })
+
+  it('deve clicar em São Paulo no mapa e exibir sua informação', () => {
+    cy.get('#BR-SP').click()
+    cy.wait(5000)
+    cy.screenshot('map-state-SP-clicked')
+    cy.get('.tooltip').should('exist') 
+    cy.get('.tooltip').should('be.visible') 
+   
+  })
+
+  it('deve clicar no Rio de Janeiro no mapa e exibir sua informação', () => {
+    cy.get('#BR-RJ').click()
+    cy.wait(5000) 
+    cy.screenshot('map-state-RJ-clicked')
+    cy.get('.tooltip').should('exist') 
+    cy.get('.tooltip').should('be.visible') 
+   
+  })
+
+ 
+  it('deve clicar no Minas Gerais no mapa e exibir sua informação', () => {
+    cy.get('#BR-MG').click()
+    cy.wait(3000)
+    cy.screenshot('map-state-MG-clicked')
+    cy.get('.tooltip').should('exist')
+    cy.get('.tooltip').should('be.visible')
     
   })
 
-  
-})
-
-describe('Testes do Mapa', () => {
-  it('deve clicar em um estado e exibir sua informação', () => {
-    cy.visit('/') // Verifique se a URL está correta
-    cy.get('#BR-AC').click() // Clique em um estado específico
-    cy.get('.tooltip').should('be.visible') // Verifique se o tooltip está visível
-    
+  it('deve clicar no Bahia no mapa e exibir sua informação', () => {
+    cy.get('#BR-BA').click()
+    cy.wait(5000)
+    cy.screenshot('map-state-BA-clicked')
+    cy.get('.tooltip').should('exist')
+    cy.get('.tooltip').should('be.visible')
+   
   })
 })
